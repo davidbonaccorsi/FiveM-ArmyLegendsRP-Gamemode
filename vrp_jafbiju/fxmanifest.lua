d10eef3f01d355babffae7d8ef5f7ba15ec752b6
David Bonaccorsi <bonaccorsidavid10@gmail.com>
2026-01-17 11:23:03 +0200

    Actualizar fxmanifest.lua

--- vrp_jafbiju/fxmanifest.lua
+++ vrp_jafbiju/fxmanifest.lua
@@ -7,10 +7,6 @@ dependency 'vrp'
 ui_page "html/index.html"
 ui_page_preload "yes"
 
-author "Proxy#3805"
-description "Vangelico Rob System"
-version "1.0"
-
 client_scripts({
 	"@vrp/client/Tunnel.lua",
 	"cl_*.lua",
